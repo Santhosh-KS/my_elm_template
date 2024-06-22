912eb8b49d64c8ac571530c0530e59516f6053c8
Santhosh K S <santhoshachar08@gmail.com>
2024-06-22 22:43:41 +0530

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,15 @@
+module Main exposing (main)
+
+-- import Browser
+
+import Html exposing (..)
+
+
+view : Html msg
+view =
+    Html.h1 [] [ Html.text "Hello Elm from Scratch from Santhosh" ]
+
+
+main : Html msg
+main =
+    view
